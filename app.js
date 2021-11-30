3cf01189adeb41cbf7b3f9459c26f37e45caaa85
kourosh <kourosh.elkaie@gmail.com>
2021-11-30 09:33:58 +0330

    store data on localStorage and getting again on array

--- app.js
+++ app.js
@@ -3,15 +3,35 @@ const addBtn = document.querySelector(".add_button")
 const filterBtn = document.querySelector(".filter_button")
 const addInput = document.querySelector('#add_input')
 const filterInput = document.querySelector('#filter_input')
+const addForm = document.querySelector("#add")
+const filterForm = document.querySelector('#filter')
+
+let task = []
 
 // hide/visible inputs
 addBtn.addEventListener('click',e=>{
     addInput.style.display = "block"
     filterInput.style.display = "none"
 })
-
 filterBtn.addEventListener('click',e=>{
     filterInput.style.display = "block"
     addInput.style.display = "none"
 })
 
+
+// keep data on array and DOM when we refreshed the page
+let getTask = localStorage.getItem("tasks")
+if(getTask) {
+    task = JSON.parse(getTask)
+}
+
+
+// submit for store data
+addForm.addEventListener('submit',e=>{
+    e.preventDefault()
+    let inputValue = addInput.value
+    task.push(inputValue)
+
+    // put on localStorage
+    localStorage.setItem("tasks",JSON.stringify(task))
+})
