a978eaff4dedc6a39a912249196b37e31d80278d
kourosh <kourosh.elkaie@gmail.com>
2021-12-01 09:32:46 +0330

    add remove task feature

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const filterInput = document.querySelector('#filter_input')
 const addForm = document.querySelector("#add")
 const filterForm = document.querySelector('#filter')
 const container = document.querySelector('.tasks_container')
+let flag = 1
 
 let task = []
 
@@ -13,63 +14,76 @@ let task = []
 addBtn.addEventListener('click',e=>{
     addInput.style.display = "block"
     filterInput.style.display = "none"
-
-    // focus on textInput
     addInput.focus()
 })
 filterBtn.addEventListener('click',e=>{
     filterInput.style.display = "block"
     addInput.style.display = "none"
-
-    // focus on textInput
     filterInput.focus()
 })
+
+
+// put data on DOM
+const dom = (data)=>{
+    container.innerHTML = ""
+    data.forEach((item,index)=>{
+        let li = document.createElement('li')
+        let p = document.createElement('p')
+        let sp = document.createElement('span')
+        p.textContent = item.todo
+        sp.innerHTML = "&times;"
+        li.appendChild(p)
+        li.appendChild(sp)
+        container.prepend(li)
+        sp.classList.add('close_button')
+        p.classList.add('para')
+    // done task stroke
+        // p.addEventListener('click',e=>{
+        //     if(item.done === true) {
+        //         // update in array
+        //         item.done = false
+        //         // update the new array in localStorage
+        //         localStorage.setItem("tasks",JSON.stringify(data))
+        //         p.style.color = 'red'
+        //         // update the new array in DOM
+        //         // dom(task)
+        //     } else {
+        //         item.done = true
+        //         localStorage.setItem("tasks",JSON.stringify(data))
+        //         p.style.color ="green"
+        //         // dom(task)    
+        //     }
+        // })
+    // delete task
+        sp.addEventListener('click',e=>{
+            // detele from array
+            data.splice(index,1 )
+            // update new array in localStorage
+            localStorage.setItem("tasks",JSON.stringify(data))
+            // update new array in DOM
+            dom(task)
+        })
+    })
+}
 
 
 // keep data on array and DOM when we refreshed the page
 let getTask = localStorage.getItem("tasks")
 if(getTask) {
     task = JSON.parse(getTask)
-    task.forEach((item)=>{
-        let li = document.createElement('li')
-        let p = document.createElement('p')
-        let sp = document.createElement('span')
-
-        p.textContent = item
-        sp.innerHTML = "&times;"
-        li.appendChild(p)
-        li.appendChild(sp)
-
-        container.prepend(li)
-    })
+    dom(task)
 }
-
 
 // submit for store data
 addForm.addEventListener('submit',e=>{
     e.preventDefault()
     let inputValue = addInput.value
-    task.push(inputValue)
-    
-    // put on localStorage
+    task.push({
+        todo: inputValue,
+        done : true
+    })
+    // =====localStorage====
     localStorage.setItem("tasks",JSON.stringify(task))
-    dom()
+    dom(task)
     addInput.value = " "
 })
-
-// put data on DOM
-const dom = ()=>{
-    container.innerHTML = ""
-    task.forEach((item)=>{
-        let li = document.createElement('li')
-        let p = document.createElement('p')
-        let sp = document.createElement('span')
-
-        p.textContent = item
-        sp.innerHTML = "&times;"
-        li.appendChild(p)
-        li.appendChild(sp)
-
-        container.prepend(li)
-    })
-}
